a11b8b2f1f6a345f6358f2929e66ab56089b7dce
elit3guzhva <nik_finger@mail.ru>
2022-03-01 23:36:45 +0300

    [fix] Fix lsp issue

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -8,6 +8,7 @@ lsp_installer.on_server_ready(function(server)
 	local opts = {
 		on_attach = require("user.lsp.handlers").on_attach,
 		capabilities = require("user.lsp.handlers").capabilities,
+        root_dir = function() return vim.loop.cwd() end,
 	}
 
     if server.name == "clangd" then
