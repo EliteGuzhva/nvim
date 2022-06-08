a1507941c7b67b7c42e854331406f54813606e4d
Гужва Никита <n.guzhva@cognitivepilot.com>
2022-06-08 14:25:38 +0300

    [fix] Finally xkb-switch works on Linux

--- lua/user/xkb-switch.lua
+++ lua/user/xkb-switch.lua
@@ -1,7 +1,10 @@
 vim.g.XkbSwitchEnabled = true
 
-if vim.fn.has("mac") then
-  vim.g.XkbSwitchLib = "/usr/local/lib/libxkbswitch.dylib"
+local mac_lib = "/usr/local/lib/libxkbswitch.dylib"
+local linux_lib = "/usr/local/lib/libg3kbswitch.so"
+
+if vim.loop.fs_stat(mac_lib) then
+    vim.g.XkbSwitchLib = mac_lib
 else
-  vim.g.XkbSwitchLib = "/usr/local/lib/libg3kbswitch.so"
+    vim.g.XkbSwitchLib = linux_lib
 end
