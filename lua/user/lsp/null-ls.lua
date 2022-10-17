7bad0326cc91408ebdc191a3446b5b6594a459e2
elit3guzhva <nik_finger@mail.ru>
2022-10-17 23:56:24 +0300

    [upd] Updated plugin settings

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,23 +1,23 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+    return
 end
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		-- C/C++
-		-- formatting.clang_format,
-		-- Lua
-		formatting.stylua,
-		-- Python
-		formatting.black.with({
-			extra_args = { "--fast", "-l", "79" },
-		}),
-		diagnostics.flake8,
+    debug = false,
+    sources = {
+        -- C/C++
+        -- formatting.clang_format,
+        -- Lua
+        formatting.stylua,
+        -- Python
+        formatting.black.with({
+            extra_args = { "--fast", "-l", "79" },
+        }),
+        diagnostics.flake8,
         diagnostics.mypy
-	},
+    },
 })
