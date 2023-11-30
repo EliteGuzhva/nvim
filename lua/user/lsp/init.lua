61e0822c95c96ad9f80563bfb568a4a22640902a
elit3guzhva <nik_finger@mail.ru>
2023-11-30 13:41:56 +0300

    [upd] dartls support

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,7 +1,24 @@
-local status_ok, _ = pcall(require, "lspconfig")
+local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
     return
 end
+
+local opts = {
+    on_attach = require("user.lsp.handlers").on_attach,
+    capabilities = require("user.lsp.handlers").capabilities,
+    root_dir = function() return vim.loop.cwd() end,
+}
+
+-- dartls
+local dartls_opts = {
+    cmd = {
+        "dart",
+        "language-server",
+        "--protocol=lsp"
+    }
+}
+dartls_opts = vim.tbl_deep_extend("force", dartls_opts, opts)
+lspconfig.dartls.setup(dartls_opts)
 
 require("user.lsp.mason-lspconfig")
 require("user.lsp.handlers").setup()
